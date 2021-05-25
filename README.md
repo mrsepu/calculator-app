5e316e27b26003679487a8bdccb7bec9a80a6d48
Miguel <mrsepu@gmail.com>
2021-05-25 18:13:07 -0400

    final

---
+++ README.md
@@ -0,0 +1,66 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+### Screenshot
+
+![screencapture-1](https://user-images.githubusercontent.com/64711777/119572448-bbd9aa80-bd80-11eb-8bde-7151e8f8c3d8.png)
+
+![screencapture-2](https://user-images.githubusercontent.com/64711777/119572530-e4fa3b00-bd80-11eb-80a2-1eede554141d.png)
+
+![screencapture-3](https://user-images.githubusercontent.com/64711777/119572575-f5121a80-bd80-11eb-8c2b-e2bff0802b5d.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Bootstrap Grid
+- Mobile-first workflow
+- Vanilla JS
+
+### What I learned
+
+I learned a lot with this project, at first sight it seamed to be easy for me, but walking through out the project I found very challenged stuff like the toggle for themes among others. It took me a while to develop every detail of the style and appearence as well as the behavior, but finally I think that the result is the one desired and I am happy with it. 
+
+### Continued development
+
+There are always many things to improve and I hope to continue working on this project to realease better versions.
+
+I want to specially improve in the toggle behavior to have a bunch of themes which users can select according to their preferences.
+
+## Author
+
+- Github - [@mrsepu](https://github.com/mrsepu)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/mrsepu)
+- Email - [mrsepu](mailto: mi.ro.se.sys@gmail.com)
---
+++ js/app.js
@@ -0,0 +1,216 @@
+//Calculator
+let value = "";
+let result = 0;
+let sum=false;
+let res=false;
+let mul=false;
+let div=false;
+let equ=false;
+let firstOp=true;
+
+$('button').click(function () {
+    $('#screen').val(valid(this.innerText));
+    if (this.innerText === '+') {
+        add();
+    } else if (this.innerText === '-') { 
+        rest();
+    } else if (this.innerText === 'X') { 
+        multiply();
+    } else if (this.innerText === '/') { 
+        divide();
+    } else if (this.innerText === '=' || this.innerText === 'Enter') {
+        equal();
+    } else if(this.innerText.toUpperCase() === 'RESET') {
+       reset();
+    } else if(this.innerText.toUpperCase() === 'DEL') {
+        del();
+    } 
+    this.blur();
+})
+
+$(document).keydown(function(e) {
+    $("#screen").val(valid(e.key));
+    if (e.key === '+') {
+        add();
+    } else if (e.key === '-') { 
+        rest();
+    } else if (e.key === '*') { 
+        multiply();
+    } else if (e.key === '/') { 
+        divide();
+    } else if (e.key === '=' || e.key === 'Enter') {
+        equal();
+    } else if (e.key.toUpperCase() ==='ESCAPE') {
+        reset();
+    } else if (e.key.toUpperCase() ==='BACKSPACE') {
+        del();
+    }
+})
+
+function valid (input) {
+    if(/[0-9\.]/.test(input)) {
+        if(input == "." && value.includes(".")) {
+            value += '';
+        } else {
+            value += input;
+        }
+    }
+    return value;
+}
+
+function add() {
+    if(div) {                      
+        result /= Number(valid());          
+    } else if(mul) {        
+        result *= Number(valid());
+    } else if(res) {
+        result -= Number(valid());
+    } else {       
+        result += Number(valid());
+    }    
+    $("#screen").val(result);
+    value = "" 
+    sum=true;
+    res=false;
+    mul=false;
+    div=false
+    firstOp=false;
+}
+
+function rest() {
+    if(firstOp) {
+        result += Number(valid());
+    } else {
+        if(div) {                      
+            result /= Number(valid());          
+        } else if(mul) {
+            result *= Number(valid());
+        } else if(sum) {
+            result += Number(valid());
+        } else {
+            result -= Number(valid());
+        }    
+    }
+    $("#screen").val(result);  
+    value = "" 
+    sum=false;
+    res=true;
+    mul=false;
+    div=false;
+    firstOp=false;
+}
+
+function multiply() {   
+    if(firstOp || equ) {
+        result += Number(valid());
+    } else {
+        if(div) {                      
+            result /= Number(valid());          
+        } else if(res) {
+            result -= Number(valid());
+        } else if(sum) {
+            result += Number(valid());
+        } else {                      
+            result *= Number(valid());          
+        }   
+    }
+    $("#screen").val(result);
+    value = "" 
+    sum=false;
+    res=false;
+    mul=true;
+    div=false;
+    firstOp=false;
+    equ=false;
+}
+
+function divide() {   
+    if(firstOp || equ) {
+        result += Number(valid());
+    } else {
+        if(res) {
+            result -= Number(valid());
+        } else if(sum) {
+            result += Number(valid());
+        } else if(mul) {                    
+            result *= Number(valid());          
+        } else {                    
+            result /= Number(valid());          
+        }
+    }
+    $("#screen").val(result);
+    value = "" 
+    sum=false;
+    res=false;
+    mul=false;
+    div=true;
+    firstOp=false;
+    equ=false;
+}
+
+function equal() {
+    if(sum) {
+        result += Number(valid());
+    }
+    if(res) {
+        result -= Number(valid());
+    }
+    if(mul) {
+        result *= Number(valid());
+    }
+    if(div) {                      
+        result /= Number(valid());          
+    }
+    $("#screen").val(result);
+    value="";
+    sum=false;
+    res=false;
+    mul=false;
+    div=false;
+    firstOp=false;
+    equ=true;
+}
+
+function reset() {
+    $('#screen').val("");
+    value = "";
+    result = 0;
+    sum=false;
+    res=false;
+    mul=false;
+    div=false;
+    equ=false;
+    firstOp=true;
+}
+
+function del () {
+    value=value.replace(value.charAt(value.length-1),"");
+    $('#screen').val(value);
+}
+
+//Theme
+const r = document.querySelector(':root');
+const themeSelector=document.querySelector('input[type=range]')
+themeSelector.addEventListener ('input' , () => {
+    setTheme(themeSelector.value);    
+})
+
+function setTheme(theme) {
+    /* Backgorunds */
+    r.style.setProperty('--mainBackground', theme==='1' ? 'hsl(222, 26%, 31%)' : theme==='2' ? 'hsl(0, 0%, 90%)' : 'hsl(268, 75%, 9%)');   
+    r.style.setProperty('--toggleAndKeypadBackground', theme==='1' ? 'hsl(223, 31%, 20%)' : theme==='2' ? 'hsl(0, 5%, 81%)' : 'hsl(268, 71%, 12%)');   
+    r.style.setProperty('--screenBackground', theme==='1' ? 'hsl(224, 36%, 15%)' : theme==='2' ? 'hsl(0, 0%, 93%)' : 'hsl(268, 71%, 12%)');  
+
+    /* keys */
+    r.style.setProperty('--keyBackgroundResDel', theme==='1' ? 'hsl(225, 21%, 49%)' : theme==='2' ? 'hsl(185, 42%, 37%)' : 'hsl(281, 89%, 26%)'); 
+    r.style.setProperty('--keyShadowResDel', theme==='1' ? 'hsl(224, 28%, 35%)' : theme==='2' ? 'hsl(185, 58%, 25%)' : 'hsl(285, 91%, 52%)');
+    r.style.setProperty('--keyBackgroundTogIqual', theme==='1' ? 'hsl(6, 63%, 50%)' : theme==='2' ? 'hsl(25, 98%, 40%)' : 'hsl(176, 100%, 44%)'); 
+    r.style.setProperty('--keyShadowTogIgual', theme==='1' ? 'hsl(6, 70%, 34%)' : theme==='2' ? 'hsl(25, 99%, 27%)' : 'hsl(177, 92%, 70%)'); 
+    r.style.setProperty('--keyBackground', theme==='1' ? 'hsl(30, 25%, 89%)' : theme==='2' ? 'hsl(45, 7%, 89%)' : 'hsl(268, 47%, 21%)'); 
+    r.style.setProperty('--keyShadow', theme==='1' ? 'hsl(28, 16%, 65%)' : theme==='2' ? 'hsl(35, 11%, 61%)' : 'hsl(290, 70%, 36%)');
+
+    /* Text */
+    r.style.setProperty('--text1', theme==='1' ? 'hsl(221, 14%, 31%)' : theme==='2' ? 'hsl(60, 10%, 19%)' : 'hsl(52, 100%, 62%)'); 
+    r.style.setProperty('--text2', theme==='1' ? '#fff' : theme==='2' ? '#fff' : '#fff'); 
+    r.style.setProperty('--text3', theme==='1' ? '#fff' : theme==='2' ? 'hsl(60, 10%, 19%)' : 'hsl(52, 100%, 62%)'); 
+}
